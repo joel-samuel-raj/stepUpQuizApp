60e706cbe27375ec5b46a52748d6c8a86e579f34
joel-samuel-raj <80976002+joel-samuel-raj@users.noreply.github.com>
2022-03-07 23:46:23 +0530

    addedLoginUi

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react'
-import { AppBar, Box, Button, Drawer, IconButton, List, ListItem, ListItemIcon, ListItemText, Modal, Toolbar, Typography } from '@mui/material'
+import { AppBar, Box, Button, Drawer, IconButton, InputAdornment, List, ListItem, ListItemIcon, ListItemText, Modal, TextField, Toolbar, Typography } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faClose } from '@fortawesome/free-solid-svg-icons'
+import { faUser, faClose, faLock, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import Link from 'next/link'
 
 export default function Navbar () {
     const [ drawer, setDrawer ] = React.useState( false )
-    const [modal, setModal] = React.useState( false )
+    const [ modal, setModal ] = React.useState( false )
+    const [ createModal, setCreateModal ] = React.useState( false )
     return (
         <>
             <Box sx={ { flexGrow: 1 } }>
@@ -26,31 +27,79 @@ export default function Navbar () {
                         <Typography variant="h6" component="div" sx={ { flexGrow: 1 } }>
                             StepUp Quiz
                         </Typography>
-                        <Button onClick={() => setModal(true)} color="inherit">Login</Button>
+                        <Button onClick={ () => setModal( true ) } color="inherit">Login</Button>
                     </Toolbar>
                 </AppBar>
             </Box>
             <Box>
-                <Drawer anchor="left" open={ drawer } onClose={() => setDrawer(false)}>
-                    <List sx={{width : "67vw", padding : "1rem"}}>
+                <Drawer anchor="left" open={ drawer } onClose={ () => setDrawer( false ) }>
+                    <List sx={ { width: "67vw", padding: "1rem" } }>
                         <Link href="/Profile">
                             <ListItem alignItems="flex-start" button>
                                 <ListItemIcon>
-                                    <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
+                                    <FontAwesomeIcon icon={ faUser }></FontAwesomeIcon>
                                 </ListItemIcon>
                                 <ListItemText> My Profile </ListItemText>
                             </ListItem>
-                        </Link> 
+                        </Link>
                     </List>
                 </Drawer>
             </Box>
-            <Modal className="flex justify-center items-center p-4" open={modal} onClose={() => setModal(false)}>
+            <Modal className="flex justify-center items-center p-4" open={ modal } onClose={ () => setModal( false ) }>
                 <Box className="relative bg-white py-12 px-16 rounded">
-                    <IconButton className="p-2 w-6 h-6 cursor-pointer bg-red-500 hover:bg-red-600 rounded absolute top-2 right-2" onClick={() => setModal(false)}>
-                        <FontAwesomeIcon className="text-white text-lg" icon={faClose}></FontAwesomeIcon>
+                    <IconButton className="p-2 w-6 h-6 cursor-pointer bg-red-500 hover:bg-red-600 rounded absolute top-2 right-2" onClick={ () => setModal( false ) }>
+                        <FontAwesomeIcon className="text-white text-lg" icon={ faClose }></FontAwesomeIcon>
                     </IconButton>
-                    <h3 className="mb-4"> Sign up with google </h3>
-                    <Button variant="contained" className="block w-[75%] mx-auto p-4"> Login with Google </Button>
+                    <TextField color="primary" className="w-full my-4" InputProps={ {
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <FontAwesomeIcon icon={ faEnvelope }></FontAwesomeIcon>
+                            </InputAdornment>
+                        ),
+                    } } label="Email" required type="email" variant="outlined" />
+                    <TextField color="primary" className="w-full" InputProps={ {
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <FontAwesomeIcon icon={ faLock }></FontAwesomeIcon>
+                            </InputAdornment>
+                        ),
+                    } } label="Password" required type="password" variant="outlined" />
+                    <Button variant="contained" className="block w-full mx-auto p-1 my-4"> Login</Button>
+                    <div className="w-full flex justify-between my-4">
+                        <p className="cursor-pointer text-sm text-red-800"> Forgot Password </p>
+                        <p onClick={() => {setModal(false); setCreateModal(true)}} className="cursor-pointer text-sm text-blue-800"> Create Account </p>
+                    </div>
+                    <Button color="secondary" variant="contained" className="block w-full mx-auto p-1"> <span> Login with Google </span> </Button>
+                </Box>
+            </Modal>
+            
+            <Modal className="flex justify-center items-center p-4" open={ createModal } onClose={ () => setModal( false ) }>
+                <Box className="relative bg-white py-12 px-16 rounded">
+                    <IconButton className="p-2 w-6 h-6 cursor-pointer bg-red-500 hover:bg-red-600 rounded absolute top-2 right-2" onClick={ () => setCreateModal( false ) }>
+                        <FontAwesomeIcon className="text-white text-lg" icon={ faClose }></FontAwesomeIcon>
+                    </IconButton>
+                    <TextField color="primary" className="w-full my-4" InputProps={ {
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <FontAwesomeIcon icon={ faUser }></FontAwesomeIcon>
+                            </InputAdornment>
+                        ),
+                    } } label="Name" required type="text" variant="outlined" />
+                    <TextField color="primary" className="w-full" InputProps={ {
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <FontAwesomeIcon icon={ faEnvelope }></FontAwesomeIcon>
+                            </InputAdornment>
+                        ),
+                    } } label="Email" required type="email" variant="outlined" />
+                     <TextField color="primary" className="w-full mt-4" InputProps={ {
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <FontAwesomeIcon icon={ faLock }></FontAwesomeIcon>
+                            </InputAdornment>
+                        ),
+                    } } label="Password" required type="password" variant="outlined" />
+                    <Button variant="contained" className="block w-full mx-auto p-1 my-4"> Create Account</Button>
                 </Box>
             </Modal>
         </>
